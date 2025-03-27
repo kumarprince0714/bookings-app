f2a2352e7f03a4dd0ebd60d26639233432eb2101
kumarprince0714 <prince.kumar@tarams.com>
2025-03-27 17:21:37 +0530

    Initialized the project

---
+++ bookingApp/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-blue-500">
+        <p>Bookings app</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
